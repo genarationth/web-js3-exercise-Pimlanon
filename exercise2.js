53efbcd9ae1bace60bd424469b3e28686301a5fc
Pimlanon <125030381+Pimlanon@users.noreply.github.com>
2023-03-14 07:59:37 +0000

    completed part2

--- exercise2.js
+++ exercise2.js
@@ -1,5 +1,17 @@
 //part1
-const EdsFriends = (name1, name2, name3) => {
+const edsFriends = (name1, name2, name3) => {
     console.log(`Welcome ${name1}, ${name2}, ${name3}.`);
 }
-EdsFriends('Tinky Wink','Dipsy','Lala'); //Welcome Tinky Wink, Dipsy, Lala.
+edsFriends('Tinky Wink','Dipsy','Lala'); //Welcome Tinky Wink, Dipsy, Lala.
+
+//part2
+function Edage(birthYear) {
+    const age = 2023 - birthYear;
+    return age
+}
+
+console.log(Edage(1990));
+//33
+
+
+//part3
