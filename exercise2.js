26f4506612909f2ad0758ea7f17b596cc0db9ac3
Pimlanon <125030381+Pimlanon@users.noreply.github.com>
2023-03-14 08:00:40 +0000

    completed part3

--- exercise2.js
+++ exercise2.js
@@ -15,3 +15,8 @@ console.log(Edage(1990));
 
 
 //part3
+const edsFriendsAndAge = (name_1, name_2, name_3, age_1, age_2, age_3) => {
+    console.log(`Welcome ${name_1}, you are ${age_1}. Welcome ${name_2}, you are ${age_2}.Welcome ${name_3}, you are ${age_3}.`);
+}
+edsFriendsAndAge('Tinky Winky', 'Dipsy', 'Lala', 28, 25, 30);
+//Welcome Tinky Winky, you are 28. Welcome Dipsy, you are 25.Welcome Lala, you are 30.
